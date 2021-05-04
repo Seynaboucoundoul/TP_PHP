195c25c268dc36ab39a63246818ff64ae5887468
OmzoPro <ndiayeomarthiate@gmail.com>
2021-05-04 15:00:59 +0000

    modification des fichiers index.php, index.html et affichage.php

--- index.php
+++ index.php
@@ -1,7 +1,3 @@
 <?php
-                if(isset($_GET['erreur'])){
-                    $err = $_GET['erreur'];
-                    if($err==1 || $err==2)
-                        echo "<p style='color:red'>Utilisateur ou mot de passe incorrect</p>";
-                }
-                ?>
+include "index.html";
+?>

2046e2cabe195a358972e6f0f07500c2c85e62a9
Seynaboucoundoul <naaboujaa@gmail.com>
2021-05-04 11:39:32 +0000

    modification de index.php

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+
+<html>
+
+    <head>
+
+      <meta charset="utf-8" />
+
+        <title> Ma deuxième page </title>
+
+    </head>
+
+	<body>
+              <p>
+
+            Bienvenue sur mon super site !<br />
+            Vous allez adorer ici.
+
+             </p>
+
+       </body>
+
+</html>
